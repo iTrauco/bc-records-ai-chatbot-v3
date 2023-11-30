54193724763e3c3c354a8262c8e1620580447bce
Christopher Trauco <chris@trau.co>
2023-11-29 21:23:52 -0500

    initial commit

---
+++ etl.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+gcloud projects list | sed 's/[:blank:]+/,/g' > projects.csv
+
+
+python3 agents.py | tr -d '"' | tr -d '{' | tr -d '}' | awk '{ if($0 !~ /^[[:space:]]*$/) print $0 }' | sed '$!s/$/,/' | sed 's/^ *//g' > agents.csv
